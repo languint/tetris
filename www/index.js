cd370a05a0eb96aebbbed4b7bbef87a926bd96e1
languint <longuint@proton.me>
2025-07-12 21:43:01 -0500

    fix: async runtime

--- www/index.js
+++ www/index.js
@@ -1,47 +1,52 @@
+import init, { Game } from "wasm-tetris";
 
-import { Game } from "wasm-tetris";
+async function run() {
+  await init();
 
-const game = new Game();
+  const game = new Game();
 
-window.onresize = () => game.resize();
+  window.onresize = () => game.resize();
 
-let lastTime = 0;
-const renderLoop = (currentTime) => {
-  const deltaTime = currentTime - lastTime;
-  lastTime = currentTime;
+  let lastTime = 0;
+  const renderLoop = (currentTime) => {
+    const deltaTime = currentTime - lastTime;
+    lastTime = currentTime;
 
-  game.tick(deltaTime);
+    game.tick(deltaTime);
+
+    requestAnimationFrame(renderLoop);
+  };
+
+  addEventListener("keydown", (e) => {
+    switch (e.code) {
+      case "KeyR":
+      case "ArrowUp":
+        game.rotate_current_piece();
+        break;
+      case "KeyA":
+      case "ArrowLeft":
+        game.move_cursor_left();
+        break;
+      case "KeyD":
+      case "ArrowRight":
+        game.move_cursor_right();
+        break;
+      case "Space":
+        game.hard_drop_current_piece();
+        break;
+      case "KeyC":
+      case "ShiftLeft":
+      case "ShiftRight":
+        game.hold_piece();
+        break;
+      case "KeyS":
+      case "ArrowDown":
+        game.soft_drop();
+        break;
+    }
+  });
 
   requestAnimationFrame(renderLoop);
-};
+}
 
-addEventListener("keydown", (e) => {
-  switch (e.code) {
-    case "KeyR":
-    case "ArrowUp":
-      game.rotate_current_piece();
-      break;
-    case "KeyA":
-    case "ArrowLeft":
-      game.move_cursor_left();
-      break;
-    case "KeyD":
-    case "ArrowRight":
-      game.move_cursor_right();
-      break;
-    case "Space":
-      game.hard_drop_current_piece();
-      break;
-    case "KeyC":
-    case "ShiftLeft":
-    case "ShiftRight":
-      game.hold_piece();
-      break;
-    case "KeyS":
-    case "ArrowDown":
-      game.soft_drop();
-      break;
-  }
-})
-
-requestAnimationFrame(renderLoop);
+run().catch(e => console.error("Error in run() function:", e));
